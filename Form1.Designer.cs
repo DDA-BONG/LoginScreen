aa3575e3f195330e42ccf69761f68b97d62176d2
형규 윤 <yhk8835@naver.com>
2026-04-02 10:09:50 +0900

    과제 1 코딩과 테스트 완료 기본 UI및 컨트롤 배치 Placeholder로 입력창 안내 기능 구현 아이디 및 패스워드 처리기능 구현 로그인 실패, 성공에 따른 메시지 표시 구현

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,109 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            lblId = new Label();
+            lblPassword = new Label();
+            txtId = new TextBox();
+            txtPassword = new TextBox();
+            btnLogin = new Button();
+            lbl_main = new Label();
+            SuspendLayout();
+            // 
+            // lblId
+            // 
+            lblId.AutoSize = true;
+            lblId.Font = new Font("맑은 고딕", 10F);
+            lblId.Location = new Point(86, 182);
+            lblId.Margin = new Padding(4, 0, 4, 0);
+            lblId.Name = "lblId";
+            lblId.Size = new Size(61, 23);
+            lblId.TabIndex = 0;
+            lblId.Text = "아이디";
+            // 
+            // lblPassword
+            // 
+            lblPassword.AutoSize = true;
+            lblPassword.Font = new Font("맑은 고딕", 10F);
+            lblPassword.Location = new Point(78, 253);
+            lblPassword.Margin = new Padding(4, 0, 4, 0);
+            lblPassword.Name = "lblPassword";
+            lblPassword.Size = new Size(78, 23);
+            lblPassword.TabIndex = 1;
+            lblPassword.Text = "패스워드";
+            // 
+            // txtId
+            // 
+            txtId.Font = new Font("맑은 고딕", 20F);
+            txtId.Location = new Point(155, 171);
+            txtId.Margin = new Padding(4);
+            txtId.Name = "txtId";
+            txtId.Size = new Size(258, 52);
+            txtId.TabIndex = 2;
+            txtId.Enter += txtId_Enter;
+            txtId.Leave += txtId_Leave;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Font = new Font("맑은 고딕", 20F);
+            txtPassword.Location = new Point(155, 238);
+            txtPassword.Margin = new Padding(4);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(258, 52);
+            txtPassword.TabIndex = 3;
+            txtPassword.Enter += txtPassword_Enter;
+            txtPassword.Leave += txtPassword_Leave;
+            // 
+            // btnLogin
+            // 
+            btnLogin.BackColor = SystemColors.ActiveCaption;
+            btnLogin.ForeColor = Color.FromArgb(0, 0, 192);
+            btnLogin.Location = new Point(187, 320);
+            btnLogin.Margin = new Padding(4);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(185, 43);
+            btnLogin.TabIndex = 4;
+            btnLogin.Text = "로그인";
+            btnLogin.UseVisualStyleBackColor = false;
+            btnLogin.Click += btnLogin_Click;
+            // 
+            // lbl_main
+            // 
+            lbl_main.AutoSize = true;
+            lbl_main.Font = new Font("맑은 고딕", 50F);
+            lbl_main.Location = new Point(155, 18);
+            lbl_main.Name = "lbl_main";
+            lbl_main.Size = new Size(258, 112);
+            lbl_main.TabIndex = 5;
+            lbl_main.Text = "Login";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Text = "Form1";
+            ClientSize = new Size(571, 516);
+            Controls.Add(lbl_main);
+            Controls.Add(btnLogin);
+            Controls.Add(txtPassword);
+            Controls.Add(txtId);
+            Controls.Add(lblPassword);
+            Controls.Add(lblId);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Margin = new Padding(4);
+            MaximizeBox = false;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Login Screen";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lblId;
+        private Label lblPassword;
+        private TextBox txtId;
+        private TextBox txtPassword;
+        private Button btnLogin;
+        private Label lbl_main;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,88 @@
-namespace LoginScreen
+﻿namespace LoginScreen
 {
     public partial class Form1 : Form
     {
+        private const string CorrectId = "abc";
+        private const string CorrectPassword = "123";
+        private const string IdPlaceholder = "아이디 입력";
+        private const string PasswordPlaceholder = "패스워드 입력";
+
         public Form1()
         {
             InitializeComponent();
+            SetIdPlaceholder();
+            SetPasswordPlaceholder();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string inputId = txtId.ForeColor == Color.Gray ? string.Empty : txtId.Text;
+            // 패스워드 입력란이 회색이면 빈 문자열로 간주
+            string inputPassword = txtPassword.ForeColor == Color.Gray ? string.Empty : txtPassword.Text;
+            
+            if (inputId == CorrectId && inputPassword == CorrectPassword) // 입력된 아이디와 패스워드가 모두 일치하는 경우
+            {
+                MessageBox.Show("로그인 성공", "알림", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            MessageBox.Show("로그인 실패", "알림", MessageBoxButtons.OK, MessageBoxIcon.Error); // 입력된 아이디 또는 패스워드가 하나라도 일치하지 않는 경우
+        }
+
+        private void txtId_Enter(object sender, EventArgs e)
+        {
+            if (txtId.ForeColor != Color.Gray)// 아이디 입력란이 회색이 아닌 경우 (즉, 이미 사용자가 입력한 텍스트가 있는 경우)
+            {
+                return;
+            }
+
+            txtId.Clear();
+            txtId.ForeColor = Color.Black;
+        }
+
+        private void txtId_Leave(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtId.Text))
+            {
+                return;
+            }
+
+            SetIdPlaceholder();
+        }
+
+        private void txtPassword_Enter(object sender, EventArgs e)
+        {
+            if (txtPassword.ForeColor != Color.Gray)
+            {
+                return;
+            }
+
+            txtPassword.Clear();
+            txtPassword.ForeColor = Color.Black;
+            txtPassword.UseSystemPasswordChar = true;
+        }
+
+        private void txtPassword_Leave(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtPassword.Text))
+            {
+                return;
+            }
+
+            SetPasswordPlaceholder();
+        }
+
+        private void SetIdPlaceholder()
+        {
+            txtId.Text = IdPlaceholder;
+            txtId.ForeColor = Color.Gray;
+        }
+
+        private void SetPasswordPlaceholder()
+        {
+            txtPassword.UseSystemPasswordChar = false;
+            txtPassword.Text = PasswordPlaceholder;
+            txtPassword.ForeColor = Color.Gray;
         }
     }
 }
